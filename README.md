4ad1e7f71f6c88d4a047fe9213f423bb37f22445
Evgeny 'Riketta' Chugaev <rowneg@bk.ru>
2019-07-17 03:42:02 +0300

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Java Mirror
- JavaMirror logging arguments and all logs between caller and java.exe
+ Or may be better to call it **JavaBridge**.  
+ JavaMirror logs arguments and all logs between caller and java.exe.  
+ Useful for example to create custom Minecraft launcher (at least I use it in this way).  
+### Usage
+ Put java_mirror.exe into java folder near bin folder to allow it using "bin\java.exe" (of copy "bin" folder to java_mirror.exe).  
+ After this use java_mirror.exe instead of java.exe in usual way - nothing changes.  
+ For example: "D:\java_mirror\java_mirror.exe"; "D:\java_mirror\bin\java.exe".  
+ In result log file will be saved near java_mirror.exe with name starts with java_mirror_*.txt  
+### Author
+ **Java Mirror** developed by Riketta / rowneg@bk.ru
---
+++ java_mirror/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace java_mirror
+{
+    class Program
+    {
+        static StreamWriter writer = null;
+
+        static void Main(string[] args)
+        {
+            string logfile = string.Format(Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "{0}_{1}.txt"), typeof(Program).Namespace, DateTime.Now.ToString("yyyyMMdd.HHmmss.fffffff"));
+            writer = new StreamWriter(logfile, true);
+            Log("##### JavaMirror #####");
+
+            Log("Args:");
+            string line = "";
+            foreach (var arg in args)
+            {
+                string argument = arg;
+                if (argument.StartsWith("-") && argument.Contains("=") && argument.Contains(" "))
+                {
+                    Log("Escape for next argument required");
+                    string[] temp = argument.Split(new char[] { '=' }, 2);
+                    argument = string.Format("{0}=\"{1}\"", temp[0], temp[1]);
+                }
+                line += argument + " ";
+                Log(string.Format("\t\"{0}\"", argument));
+            }
+            line = line.Trim();
+            Log(string.Format("Calling target file with args \"{0}\"", line));
+            Process process = StartProcess(Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), @"bin\java.exe"), line);
+            Log(string.Format("Process (\"{0}\") started", process.StartInfo.FileName));
+
+            Log("Waiting until application stop with 300 seconds timeout");
+            Log("Process output:");
+            process.OutputDataReceived += (sender, e) => Log(e.Data);
+            process.ErrorDataReceived += (sender, e) => Log(e.Data);
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.WaitForExit(300 * 1000);
+
+            Log("##### Done #####");
+            writer.Close();
+            Console.ReadLine();
+        }
+
+        static void Log(string message)
+        {
+            message = string.Format("[{0}] {1}", DateTime.Now.ToString("yyyyMMdd.HHmmss.fffffff"), message);
+            Console.WriteLine(message);
+            lock (writer)
+            {
+                writer.WriteLine(message);
+                writer.Flush();
+            }
+        }
+
+        public static Process StartProcess(string path, string args)
+        {
+            Process process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = path,
+                    Arguments = args,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+
+                }
+            };
+            process.Start();
+            
+            return process;
+        }
+    }
+}
